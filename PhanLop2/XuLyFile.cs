8f87cc3c6c04bd8b08d78c384e49ec4730b1d31f
Vương Thiên Phú <thienphuvuong@gmail.com>
2018-04-04 09:14:18 +0700

    update

---
+++ PhanLop2/XuLyFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PhanLop
+{
+    class XuLyFile
+    {
+        XuLyFile()
+        {
+           
+        }
+
+        public static int docRound()
+        {
+            using (StreamReader sr = new StreamReader("round.txt"))
+            {
+                return Int32.Parse(sr.ReadLine());
+            }
+        }
+
+        public static int knn()
+        {
+            using (StreamReader sr = new StreamReader("knn.txt"))
+            {
+                return Int32.Parse(sr.ReadLine());
+            }
+        }
+    }
+}
