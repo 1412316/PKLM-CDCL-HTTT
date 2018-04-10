720bafeba4c12c0b697e2256fc314a479bfa2707
Đặng Nhật Minh <nhatminhdang1996@gmail.com>
2018-04-10 10:00:04 +0700

    PhanLop

---
+++ PhanLop/DocumentClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanLop
+{
+    class DocumentClass
+    {
+        String name { get; set; }
+        int count { get; set; }
+
+        DocumentClass(String name, int count)
+        {
+            this.name = name;
+            this.count = count;
+        }
+    }
+}
