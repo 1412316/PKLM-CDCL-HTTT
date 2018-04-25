360338c009e009f4cb747eaf89f6707c5d7d1773
1412274-NHKim <nhkim1496@gmail.com>
2018-04-25 20:31:27 +0700

    Tinh may cai chi so

---
+++ PhanLop/PhanLop/Document.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PhanLop
+{
+    class Document
+    {
+        public List<Feature> featureList { get; set; }
+        public String className { get; set; }
+        public int index { get; set; }
+
+        public Document()
+        {
+            this.featureList = new List<Feature>();
+            this.className = "";
+            this.index = -1;
+        }
+
+        public int timIndexCuaFeatureTrongDocument(String feature)
+        {
+            return this.featureList.FindIndex(x => x.feature == feature);
+        }
+
+        public void tangWeightChoFeatureCuaDocument(int featureIndex)
+        {
+            this.featureList.ElementAt(featureIndex).tangWeight();
+        }
+
+        public void themFeatureVaoDocument(String feature)
+        {
+            featureList.Add(new Feature(feature, 1));
+        }
+
+        public void themfeature(String feature, int weigth)
+        {
+            featureList.Add(new Feature(feature, weigth));
+        }
+
+        //Tìm feature xuất hiện nhiều nhất trong Document
+        public static double timSoLanXuatHienNhieuNhat(Document document)
+        {
+            double max = 0;
+
+            document.featureList.ForEach(delegate (Feature f)
+            {
+                if (f.weight > max)
+                {
+                    max = f.weight;
+                }
+            });
+
+            return max;
+        }
+
+        //Tinh so luong van ban co tu feature i bat ki 
+        public static int DemDocChuaI(string i, List<Document> documentlist)
+        {
+            int count = 0;
+            documentlist.ForEach(delegate (Document x)
+            {
+                x.featureList.ForEach(delegate (Feature y)
+                {
+                    if (y.feature == i)
+                    {
+                        count++;
+                    }
+                });
+            });
+            return count;
+        }
+
+        public static double timWeightCuaFeatureTrongDocument(Document document, String feature)
+        {
+            double weight = 0;
+
+            document.featureList.ForEach(delegate (Feature f)
+            {
+                if (f.feature == feature)
+                {
+                    weight = f.weight;
+                }
+            });
+
+            return weight;
+        }
+
+        public static List<Document> gopDocuments(List<Class> classList)
+        {
+            List<Document> documentList = new List<Document>();
+
+            classList.ForEach(delegate (Class c)
+            {
+                c.documentList.ForEach(delegate (Document d)
+                {
+                    documentList.Add(d);
+                });
+            });
+
+            return documentList;
+        }
+
+        public static List<Document> layCacDocuments(String filePath)
+        {
+            List<Document> documentList = new List<Document>();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                String line = null;
+                int j = 0; //Cho biết document thứ bao nhiêu trong documentList
+
+                //Mỗi một line (dòng) là một document
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Thêm một document rỗng vào danh sách documents
+                    documentList.Add(new Document());
+                    documentList.ElementAt(j).index = j;
+
+                    //Tạo ra các feature từ line mới đọc (dấu khoảng cách ngăn cách 2 features)
+                    String[] features = line.Split(' ');
+
+                    //Với mỗi feature kiem tra xem da co feature nay chua. Neu co roi thi tang trong so len. 
+                    for (int i = 0; i < features.Length; i++)
+                    {
+                        //Tìm index của feature (tìm xem có feature đó trong document chưa)
+                        int featureIndex = documentList.ElementAt(j).timIndexCuaFeatureTrongDocument(features[i]);
+
+                        if (featureIndex >= 0)
+                        {
+                            //Nếu có rồi thì tăng weight cho feature đó trong document này
+                            documentList.ElementAt(j).tangWeightChoFeatureCuaDocument(featureIndex);
+                        }
+                        else
+                        {
+                            //Nếu chưa có thì thêm feature đó vào document này
+                            documentList.ElementAt(j).themFeatureVaoDocument(features[i]);
+                        }
+                    }
+
+                    j++;
+                }
+            }
+
+            return documentList;
+        }
+
+        public static String phanLop(Document searchedDocument, int numberOfReturnDocuments, int numberOfDocuments, List<Document> documentListOfAllClasses, List<Class> classList)
+        {
+            Console.WriteLine("=========Phan Lop===========");
+            Document searchedDocument_tfidf = new Document();
+            List<FeatureIDF> featureList = new List<FeatureIDF>();
+
+            using (StreamReader sr = new StreamReader("featureList.txt"))
+            {
+                String line = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    String[] feature = line.Split(' ');
+
+                    featureList.Add(new FeatureIDF(feature[0], double.Parse(feature[1])));
+                }
+            }
+
+            featureList.ForEach(delegate (FeatureIDF f)
+            {
+                double tf = (double)timWeightCuaFeatureTrongDocument(searchedDocument, f.feature) / timSoLanXuatHienNhieuNhat(searchedDocument);
+                searchedDocument_tfidf.featureList.Add(new Feature(f.feature, Math.Round(tf * f.idf, XuLyFile.docRound())));
+            });
+
+            List<double[]> bow_tfidf = BOW.docBOW_tfidf();
+            SimilarityMeasure[] similarityMeasures = new SimilarityMeasure[numberOfDocuments];
+            int i, j;
+
+            for (i = 0; i < numberOfDocuments; i++)
+            {
+                double value = 0;
+
+                for (j = 0; j < searchedDocument_tfidf.featureList.Count; j++)
+                {
+                    value += Math.Pow(bow_tfidf.ElementAt(i)[j] - searchedDocument_tfidf.featureList.ElementAt(j).weight, 2);
+                }
+
+                similarityMeasures[i] = new SimilarityMeasure(Math.Round(Math.Sqrt(value), XuLyFile.docRound()), i);
+            }
+
+            //Sắp xếp độ tương tự tăng dần
+            SimilarityMeasure[] sortedSimilarityMeasures = similarityMeasures.OrderBy(sm => sm.value).ToArray();
+
+            Dictionary<String, int> classesOfDocument = new Dictionary<string, int>();
+            for (int k = 0; k < numberOfReturnDocuments; k++)
+            {
+                for (int f = 0; f < documentListOfAllClasses.Count; f++)
+                {
+                    if (f == sortedSimilarityMeasures[k].index)
+                    {
+                        Console.WriteLine(documentListOfAllClasses.ElementAt(f).className + " " + documentListOfAllClasses.ElementAt(f).index);
+                        if (classesOfDocument.ContainsKey(documentListOfAllClasses.ElementAt(f).className))
+                        {
+                            classesOfDocument[documentListOfAllClasses.ElementAt(f).className]++;
+                        }
+                        else
+                        {
+                            classesOfDocument.Add(documentListOfAllClasses.ElementAt(f).className, 1);
+                        }
+                    }
+                }
+            }
+
+            int max = classesOfDocument.ElementAt(0).Value;
+            String maxClass = classesOfDocument.ElementAt(0).Key;
+            for (int t = 0; t < classesOfDocument.Count; t++)
+            {
+                if (classesOfDocument.ElementAt(t).Value > max)
+                {
+                    max = classesOfDocument.ElementAt(t).Value;
+                    maxClass = classesOfDocument.ElementAt(t).Key;
+                }
+            }
+
+            return maxClass;
+        }
+
+        public static List<String> layChuoiCacDocuments(String input)
+        {
+            using (StreamReader sr = new StreamReader(input))
+            {
+                String line = null;
+                List<String> lstr = new List<string>();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lstr.Add(line);
+                }
+
+                return lstr;
+            }
+        }
+
+        //====================Thêm vào====================
+        public static List<Document> layCacDocumentsDaPhanClass(String filePath)
+        {
+            List<Document> documentList = new List<Document>();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                String line = null;
+                int j = 0; //Cho biết document thứ bao nhiêu trong documentList
+
+                //Mỗi một line (dòng) là một document
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Thêm một document rỗng vào danh sách documents
+                    documentList.Add(new Document());
+                    documentList.ElementAt(j).index = j;
+
+                    //Tạo ra các feature từ line mới đọc (dấu khoảng cách ngăn cách 2 features)
+                    String[] features = line.Split(' ');
+
+                    //Với mỗi feature kiem tra xem da co feature nay chua. Neu co roi thi tang trong so len. 
+                    for (int i = 0; i < features.Length; i++)
+                    {
+                        if (i == features.Length - 1)
+                        {
+                            documentList.ElementAt(j).className = features[i];
+                        }
+                        else
+                        {
+                            //Tìm index của feature (tìm xem có feature đó trong document chưa)
+                            int featureIndex = documentList.ElementAt(j).timIndexCuaFeatureTrongDocument(features[i]);
+
+                            if (featureIndex >= 0)
+                            {
+                                //Nếu có rồi thì tăng weight cho feature đó trong document này
+                                documentList.ElementAt(j).tangWeightChoFeatureCuaDocument(featureIndex);
+                            }
+                            else
+                            {
+                                //Nếu chưa có thì thêm feature đó vào document này
+                                documentList.ElementAt(j).themFeatureVaoDocument(features[i]);
+                            }
+                        }
+                    }
+
+                    j++;
+                }
+            }
+
+            return documentList;
+        }
+
+        public static String phanLopCosine(Document searchedDocument, int numberOfReturnDocuments, int numberOfDocuments, List<Document> documentListOfAllClasses, List<Class> classList)
+        {
+            Console.WriteLine("=========Phan Lop Cosine===========");
+            Document searchedDocument_tfidf = new Document();
+            List<FeatureIDF> featureList = new List<FeatureIDF>();
+
+            using (StreamReader sr = new StreamReader("featureList.txt"))
+            {
+                String line = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    String[] feature = line.Split(' ');
+
+                    featureList.Add(new FeatureIDF(feature[0], double.Parse(feature[1])));
+                }
+            }
+
+            featureList.ForEach(delegate (FeatureIDF f)
+            {
+                double tf = (double)timWeightCuaFeatureTrongDocument(searchedDocument, f.feature) / timSoLanXuatHienNhieuNhat(searchedDocument);
+                searchedDocument_tfidf.featureList.Add(new Feature(f.feature, Math.Round(tf * f.idf, XuLyFile.docRound())));
+            });
+
+            List<double[]> bow_tfidf = BOW.docBOW_tfidf();
+            SimilarityMeasure[] similarityMeasures = new SimilarityMeasure[numberOfDocuments];
+            int i, j;
+
+            for (i = 0; i < numberOfDocuments; i++)
+            {
+                double value1 = 0;
+                double value2 = 0;
+                double value3 = 0;
+                double value = 0;
+
+                for (j = 0; j < searchedDocument_tfidf.featureList.Count; j++)
+                {
+                    value1 += bow_tfidf.ElementAt(i)[j] * searchedDocument_tfidf.featureList.ElementAt(j).weight;
+                    value2 += Math.Pow(bow_tfidf.ElementAt(i)[j], 2);
+                    value3 += Math.Pow(searchedDocument_tfidf.featureList.ElementAt(j).weight, 2);
+                }
+
+                value = (double)value1 / (Math.Sqrt(value2) * Math.Sqrt(value3));
+
+                similarityMeasures[i] = new SimilarityMeasure(Math.Round(Math.Sqrt(value), XuLyFile.docRound()), i);
+            }
+
+            //Sắp xếp độ tương tự giảm dần
+            SimilarityMeasure[] sortedSimilarityMeasures = similarityMeasures.OrderByDescending(sm => sm.value).ToArray();
+
+            Dictionary<String, int> classesOfDocument = new Dictionary<string, int>();
+            for (int k = 0; k < numberOfReturnDocuments; k++)
+            {
+                for (int f = 0; f < documentListOfAllClasses.Count; f++)
+                {
+                    if (f == sortedSimilarityMeasures[k].index)
+                    {
+                        Console.WriteLine(documentListOfAllClasses.ElementAt(f).className + " " + documentListOfAllClasses.ElementAt(f).index);
+                        if (classesOfDocument.ContainsKey(documentListOfAllClasses.ElementAt(f).className))
+                        {
+                            classesOfDocument[documentListOfAllClasses.ElementAt(f).className]++;
+                        }
+                        else
+                        {
+                            classesOfDocument.Add(documentListOfAllClasses.ElementAt(f).className, 1);
+                        }
+                    }
+                }
+            }
+
+            int max = classesOfDocument.ElementAt(0).Value;
+            String maxClass = classesOfDocument.ElementAt(0).Key;
+            for (int t = 0; t < classesOfDocument.Count; t++)
+            {
+                if (classesOfDocument.ElementAt(t).Value > max)
+                {
+                    max = classesOfDocument.ElementAt(t).Value;
+                    maxClass = classesOfDocument.ElementAt(t).Key;
+                }
+            }
+
+            return maxClass;
+        }
+
+        public static int demSoDocument(String filePath)
+        {
+            List<Document> listDoc = Document.layCacDocumentsDaPhanClass(filePath);
+            return listDoc.Count;
+        }
+
+    }
+}
---
+++ PhanLop/PhanLop/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PhanLop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nhập lựa chọn
+            Console.WriteLine("====Nhap so 0 đe phan lop theo Euclidean Similarity====");
+            Console.WriteLine("====Nhap so 1 đe phan lop theo Cosine Similarity====");
+            Console.WriteLine("Nhap vao lua chon: ");
+            string choice = Console.ReadLine();
+            int choicenum = Int32.Parse(choice);
+
+            String preprocessedFilePath = "C:\\Users\\nhkim\\Documents\\Visual Studio 2017\\Projects\\PhanLop\\PhanLop\\bin\\Debug\\preprocessedClasses\\";
+            String[] filePaths = Directory.GetFiles("C:\\Users\\nhkim\\Documents\\Visual Studio 2017\\Projects\\PhanLop\\PhanLop\\bin\\Debug\\classes", "*.txt");
+            List<Class> classList = new List<Class>();
+
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                Class c = new Class();
+                c.className = filePaths[i].Split('\\').Last().Split('.').First();
+                TienXuLy.tienXuLy(filePaths[i], preprocessedFilePath + c.className + ".txt");
+                c.layCacDocuments(preprocessedFilePath + c.className + ".txt", c.className);
+                classList.Add(c);
+            }
+
+            // Gộp tất cả các document của các class lại thành 1 documentList
+            List<Document> documentListOfAllClasses = Document.gopDocuments(classList);
+            //Tính bow tfidf
+            BOW.bow_tfdf(documentListOfAllClasses, "output.txt", "featureList.txt");
+
+            // Tiền xử lý các document cần phan lớp
+            TienXuLy.tienXuLy("test.txt", "preprocessedTest.txt");
+
+            // Các document cần được phân lớp
+            List<Document> testList = Document.layCacDocuments("preprocessedTest.txt");
+
+            if (choicenum == 0)
+            {
+                testList.ForEach(delegate (Document d)
+                {
+                    d.className = Document.phanLop(d, XuLyFile.knn(), documentListOfAllClasses.Count, documentListOfAllClasses, classList);
+                    Console.WriteLine(d.className);
+                    Console.WriteLine(d.index);
+                });
+            }
+
+            if (choicenum == 1)
+            {
+                testList.ForEach(delegate (Document d)
+                {
+                    d.className = Document.phanLopCosine(d, XuLyFile.knn(), documentListOfAllClasses.Count, documentListOfAllClasses, classList);
+                    Console.WriteLine(d.className);
+                    Console.WriteLine(d.index);
+                });
+            }
+
+            List<String> testDocuments = Document.layChuoiCacDocuments("test.txt");
+
+            using (StreamWriter sw = new StreamWriter("result.txt"))
+            {
+                for (int i = 0; i < testDocuments.Count; i++)
+                {
+                    testList.ForEach(delegate (Document d)
+                    {
+                        if (d.index == i)
+                        {
+                            sw.WriteLine(testDocuments.ElementAt(i) + "  " + d.className);
+                        }
+                    });
+                }
+            }
+
+            //=========================================Phần tính thông tin của các class=========================================
+            List<ClassInfo> listClass = ClassInfo.ThongTinClass("true.txt", "result.txt");
+
+            for (int k = 0; k < listClass.Count; k++)
+            {
+                Console.WriteLine("==========Class " + k + "==========");
+                Console.WriteLine("Ten class: " + listClass[k].ClassName);
+                Console.WriteLine("So van ban duoc phan dung vao class " + listClass[k].ClassName + ": " + listClass[k].NumberRightofClass);
+                Console.WriteLine("So van ban bi phan sai vao class " + listClass[k].ClassName + ": " + listClass[k].NumberWrongofClass);
+                Console.WriteLine("So van ban class " + listClass[k].ClassName + " bi phan sai vao class khac: " + listClass[k].NumberWrongoutClass);
+                Console.WriteLine("Do chinh xac cua class " + listClass[k].ClassName + ": " + listClass[k].Precision);
+                Console.WriteLine("Do bao phu cua class " + listClass[k].ClassName + ": " + listClass[k].Recall);
+                Console.WriteLine("Do do tong hop cua class " + listClass[k].ClassName + ": " + listClass[k].Fscore);
+                Console.WriteLine();
+            }
+
+            double f_macro = ClassInfo.F_Macro(listClass);
+            double f_micro = ClassInfo.F_Micro(listClass, Document.demSoDocument("true.txt"));
+
+            Console.WriteLine("Do do F-macro: " + f_macro);
+            Console.WriteLine("Do do F-micro: " + f_micro);
+
+        }
+    }
+}
